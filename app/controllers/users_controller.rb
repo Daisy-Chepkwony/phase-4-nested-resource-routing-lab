69a3da637c3f8f2617081061b894bf8c16a4c135
+ <+>
2023-03-21 14:10:20 +0300

    completed work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def show
     user = User.find_by(id: params[:id])
@@ -6,3 +8,6 @@ class UsersController < ApplicationController
   end
 
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items, only: [:index, :create, :show] 
+
+  # resources :items, only: [:show] do
+  #   resources :users, only: [:show, :index]
+  # end
+  end 
+  
 end
